c0152f37a062788da1b2767063f2d3567d5143ed
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2020-07-16 11:18:46 -0400

    killed wifi and bluetooth added

---
+++ power_cunsumption/time_stamped_wifi_bluetooth_disabled/run.sh
@@ -0,0 +1,22 @@
+
+sleep 5
+python3 test.py | ts > numpy_timestamped_try0.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try1.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try2.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try3.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try4.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try5.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try6.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try7.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try8.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try9.out
+
