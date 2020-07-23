b1fcbc37c21b994904411ce85ff2bb584318dbde
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2020-07-23 13:02:53 -0400

    high sampling rate added for numpy

---
+++ power_cunsumption/numpy_timestamped_high_sampling_rate/run.sh
@@ -0,0 +1,9 @@
+
+sleep 5
+python3 test.py | ts > numpy_timestamped_try0.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try1.out
+sleep 20
+python3 test.py | ts > numpy_timestamped_try2.out
+
+
