bdcd4a3f1f5937c3e967f714f7a77853dd010126
Dave Radke <dtradke@uwaterloo.ca>
2020-07-29 09:19:40 -0700

    updated scripts

--- testing_script.py
+++ testing_script.py
@@ -29,9 +29,9 @@ class runScipy(object):
         for i in funcs_to_run:
             # i should be a function call
             i(arr)
+        print("scipy")
 
     def createArray(self,h,w):
-        print("scipy")
         return np.random.rand(h,w)
 
     def f(self,x):
@@ -44,7 +44,6 @@ class runScipy(object):
         return np.array([[1 + 1.5 * (x[0] - x[1])**2, -1.5 * (x[0] - x[1])**2], [-1.5 * (x[1] - x[0])**2, 1 + 1.5 * (x[1] - x[0])**2]])
 
     def roots(self, arr=None):
-        print("scipy")
         x0 = arr.flatten()
         sol = optimize.root(self.fun, [0, 0], jac=self.jac, method='hybr')
         sol = optimize.root(self.fun, [0, 0], jac=self.jac, method='lm')
@@ -58,12 +57,10 @@ class runScipy(object):
         sol = optimize.root(self.fun, [0, 0], method='df-sane')
 
     def arrayMath(self, arr=None):
-        print("scipy")
         x = scipy.fft.fft(arr)
         x = scipy.fft.ifft(arr)
 
     def funcMinimize(self, arr=None):
-        print("scipy")
         res = optimize.minimize_scalar(self.f)
         # x = res.x
         res = optimize.minimize_scalar(self.f, bounds=(-3, -1), method='bounded')
@@ -74,7 +71,6 @@ class runScipy(object):
         # x = res.x
 
     def funcOptimize(self, arr=None):
-        print("scipy")
         from scipy.optimize import minimize, rosen, rosen_der
         x0 = arr.flatten()
         res = minimize(rosen, x0, method='Nelder-Mead', tol=1e-6)
@@ -123,13 +119,12 @@ class runNumpy(object):
         for i in funcs_to_run:
             # i should be a function call
             i(arr)
+        print("numpy")
 
     def createArray(self,h,w):
-        print("numpy")
         return np.random.rand(h,w)
 
     def arrayMath(self, arr=None):
-        print("numpy")
         x = np.subtract(arr,arr)
         x = np.add(arr,arr)
         x = np.divide(arr,arr)
@@ -138,18 +133,15 @@ class runNumpy(object):
         a = np.linspace(0,arr.shape[1], arr.shape[1])
 
     def moreMath(self, arr=None):
-        print("numpy")
         x = np.exp(arr)
         x = np.sqrt(arr)
 
     def arrayTrig(self, arr=None):
-        print("numpy")
         x = np.sin(arr)
         x = np.cos(arr)
         x = np.log(arr)
 
     def arrayAggregate(self, arr=None):
-        print("numpy")
         x = np.sum(arr)
         x = np.amin(arr)
         x = np.amax(arr)
@@ -160,12 +152,10 @@ class runNumpy(object):
         x = np.std(arr)
 
     def arraySort(self, arr=None):
-        print("numpy")
         x = np.sort(arr)
         x = np.sort(arr, axis=0)
 
     def arrayManipulation(self, arr=None):
-        print("numpy")
         x = np.transpose(arr)
         x = np.ravel(arr)
         x = arr.reshape(arr.shape[0], (-1 * arr.shape[1]))
@@ -178,24 +168,24 @@ class runPython(object):
     def __init__(self):
         super(runPython, self).__init__()
         # set functions here
-        self.funcs = [self.arrayMath,self.arrayCat,self.moreMath,self.pythonTrig]
+        self.funcs = [self.arrayMath,self.arrayCat,self.arrayTotals] #self.moreMath,self.pythonTrig
 
 
     def callSoft(self,height,width):
         func_calls = random.randint(1,len(self.funcs))
         arr = self.createArray(height,width)
         funcs_to_run = sample(self.funcs,func_calls)
+        print("python")
         for i in funcs_to_run:
             # i should be a function call
-            print("python")
             i(arr)
+        print("python")
 
     def createArray(self,h,w):
         arr = []
         for h_count in range(h):
             inner_arr = []
             for w_count in range(w):
-                print("python")
                 inner_arr.append(random.random())
             arr.append(inner_arr)
         return arr
@@ -203,7 +193,6 @@ class runPython(object):
     def arrayMath(self, arr=None):
         for i in arr:
             for j in i:
-                print("python")
                 x = j + j
                 x = j - j
                 x = j * j
@@ -211,56 +200,62 @@ class runPython(object):
 
     def arrayCat(self, arr=None):
         for i in arr:
-            print("python")
             x = i + i
 
-    def moreMath(self, arr=None):
+    def arrayTotals(self,arr=None):
+        total = 0
+        sub_total = 0
         for i in arr:
             for j in i:
-                print("python")
-                x = math.ceil(j)
-                x = math.copsign(j,-1*j)
-                x = math.fabs(j)
-                x = math.factorial(j)
-                x = math.floor(j)
-                x = math.fmod(j,j//2)
-                x = math.frexp(j)
-                x = math.isfinite(j)
-                x = math.ininf(j)
-                x = math.isnan(j)
-                x = math.idexp(j)
-                x = math.modf(j)
-                x = math.trunc(j)
-                x = math.exp(j)
-                x = math.expm1(j)
-                x = math.log1p(j)
-                x = math.log2(j)
-                x = math.log10(j)
-                x = math.pow(j,4)
-                x = math.sqrt(j)
+                total+=j
+                sub_total-=j
 
-    def pythonTrig(self, arr=None):
-        for i in arr:
-            for j in i:
-                print("python")
-                x = math.acos(j)
-                x = math.asin(j)
-                x = math.atan(j)
-                x = math.cos(j)
-                x = math.sin(j)
-                x = math.tan(j)
-                x = math.degrees(j)
-                x = math.radians(j)
-                x = math.acosh(j)
-                x = math.asinh(j)
-                x = math.atanh(j)
-                x = math.cosh(j)
-                x = math.sinh(j)
-                x = math.tanh(j)
-                x = math.erf(j)
-                x = math.erfc(j)
-                x = math.gamma(j)
-                x = math.lgamma(j)
+    # def moreMath(self, arr=None):
+    #     for i in arr:
+    #         for j in i:
+    #             x = math.ceil(j)
+    #             x = math.copsign(j,-1*j)
+    #             x = math.fabs(j)
+    #             x = math.factorial(j)
+    #             x = math.floor(j)
+    #             x = math.fmod(j,j//2)
+    #             x = math.frexp(j)
+    #             x = math.isfinite(j)
+    #             x = math.ininf(j)
+    #             x = math.isnan(j)
+    #             x = math.idexp(j)
+    #             x = math.modf(j)
+    #             x = math.trunc(j)
+    #             x = math.exp(j)
+    #             x = math.expm1(j)
+    #             x = math.log1p(j)
+    #             x = math.log2(j)
+    #             x = math.log10(j)
+    #             x = math.pow(j,4)
+    #             x = math.sqrt(j)
+
+    # def pythonTrig(self, arr=None):
+    #     for i in arr:
+    #         for j in i:
+    #             print("python")
+    #             x = math.acos(j)
+    #             x = math.asin(j)
+    #             x = math.atan(j)
+    #             x = math.cos(j)
+    #             x = math.sin(j)
+    #             x = math.tan(j)
+    #             x = math.degrees(j)
+    #             x = math.radians(j)
+    #             x = math.acosh(j)
+    #             x = math.asinh(j)
+    #             x = math.atanh(j)
+    #             x = math.cosh(j)
+    #             x = math.sinh(j)
+    #             x = math.tanh(j)
+    #             x = math.erf(j)
+    #             x = math.erfc(j)
+    #             x = math.gamma(j)
+    #             x = math.lgamma(j)
 
 
 modes = [runNumpy(), runScipy(), runPython()]
