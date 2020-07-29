991ed112e0c8133fdd57490c0a706e80b2063af5
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2020-07-29 15:17:34 -0400

    .

--- power_cunsumption/2k_per_second_sampling_numpy/run.sh
+++ power_cunsumption/2k_per_second_sampling_numpy/run.sh
@@ -1,5 +1,21 @@
 
 sleep 20
-python3 test.py | ts > numpy_timestamped_try0.out
-sleep 20
+python3 test.py | ts > numpy_timestamped_try2.out
+sleep 180
 
+
+python3 test.py | ts > numpy_timestamped_try3.out
+sleep 180
+python3 test.py | ts > numpy_timestamped_try4.out
+sleep 180
+python3 test.py | ts > numpy_timestamped_try5.out
+sleep 180
+python3 test.py | ts > numpy_timestamped_try6.out
+sleep 180
+python3 test.py | ts > numpy_timestamped_try7.out
+sleep 180
+
+python3 test.py | ts > numpy_timestamped_try8.out
+sleep 180
+python3 test.py | ts > numpy_timestamped_try9.out
+sleep 180
