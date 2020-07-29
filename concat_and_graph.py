521fcdfafd70952c7dd8d74e8226810b220e3899
Dave Radke <dtradke@uwaterloo.ca>
2020-07-29 11:35:20 -0700

    changed names and added csv

--- concat_and_graph.py
+++ concat_and_graph.py
@@ -6,9 +6,16 @@ import matplotlib.pyplot as plt
 
 
 def parseData(path):
+    # data = pd.read_csv(path + '.csv')
+    # power = np.array(data[data.columns[1:2]])*5
+    # power_time = np.array(data[data.columns[:1]])
+    path = path[:-4]
     data = pd.read_csv(path + '.csv')
-    power = np.array(data[data.columns[1:2]])*5
-    power_time = np.array(data[data.columns[:1]])
+    data = data[16:-10]
+    data = data.rename(columns={'Address': "time", 'USB0::0x2A8D::0x0101::MY59001058::0::INSTR': "power"})
+    data["power"] = pd.to_numeric(data["power"], downcast="float")
+    power = np.array(data['power'], dtype=np.float32)*5
+    power_time = np.array(data['time'], dtype=np.float32)
 
     return [power, power_time]
 
@@ -21,13 +28,16 @@ for i in sys.argv[1:]:
 
 powers = []
 times = []
-for i in file_data:
-    powers.append(i[0])
-    times.append(i[1])
+if len(file_data) > 1:
+    for i in file_data:
+        powers.append(i[0])
+        times.append(i[1])
 
+    all_power = np.squeeze(np.concatenate((powers), axis=0))
+    all_times = np.squeeze(np.concatenate((times), axis=0))
+else:
+    all_power = file_data[0][0]
+    all_times = file_data[0][1]
 
-all_power = np.squeeze(np.concatenate((powers), axis=0))
-all_times = np.squeeze(np.concatenate((times), axis=0))
-
-plt.plot(all_times[500000:501000], all_power[500000:501000])
+plt.plot(np.arange(all_power.shape[0]), all_power)
 plt.show()
