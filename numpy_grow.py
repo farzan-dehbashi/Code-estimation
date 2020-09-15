ce2ec0f4d37436f4306d46acbe430be6dc9aa9a9
Dave Radke <dtradke@uwaterloo.ca>
2020-09-14 23:26:30 -0400

    grow

--- numpy_grow.py
+++ numpy_grow.py
@@ -6,7 +6,7 @@ np.random.seed(0)
 time.sleep(1)
 
 for i in range(10):
-    arr = np.random.rand((1000*i),i,i)
+    arr = np.random.rand(1000,(i*10),(i*10))
     time.sleep(1)
     a = np.mean(arr)
-    time.sleep(2)
+    time.sleep(3)
