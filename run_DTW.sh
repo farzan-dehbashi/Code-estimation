be46b8fcec93fc52b785e05c9a104b0ddb94ca68
Dave Radke <dtradke@uwaterloo.ca>
2020-11-03 16:31:26 -0500

    sh for rq1

--- run_DTW.sh
+++ run_DTW.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-python3 DTW.py traces/noseed_numpy_random_funcs_mean/noseed_numpy_random_funcs_mean_try > rq4_mean.out
+# RQ1
+python3 DTW.py traces/numpy_random_funcs_nosleep_mean_SUBSET_long/numpy_random_funcs_nosleep_mean_SUBSET_long_try > rq1_mean.out
+python3 DTW.py traces/numpy_random_funcs_nosleep_median_SUBSET_long/numpy_random_funcs_nosleep_median_SUBSET_long_try > rq1_median.out
+python3 DTW.py traces/numpy_random_funcs_nosleep_fft_SUBSET/numpy_random_funcs_nosleep_fft_SUBSET_try > rq1_fft.out
+python3 DTW.py traces/numpy_random_funcs_nosleep_max_SUBSET_long/numpy_random_funcs_nosleep_max_SUBSET_long_try > rq1_amax.out
 
-python3 DTW.py traces/noseed_numpy_random_funcs_median/noseed_numpy_random_funcs_median_try > rq4_median.out
 
-python3 DTW.py traces/noseed_numpy_random_funcs_fft/noseed_numpy_random_funcs_fft_try > rq4_fft.out
-
-python3 DTW.py traces/noseed_numpy_random_funcs_max/noseed_numpy_random_funcs_max_try > rq4_amax.out
+# RQ4
+# python3 DTW.py traces/noseed_numpy_random_funcs_mean/noseed_numpy_random_funcs_mean_try > rq4_mean.out
+# python3 DTW.py traces/noseed_numpy_random_funcs_median/noseed_numpy_random_funcs_median_try > rq4_median.out
+# python3 DTW.py traces/noseed_numpy_random_funcs_fft/noseed_numpy_random_funcs_fft_try > rq4_fft.out
+# python3 DTW.py traces/noseed_numpy_random_funcs_max/noseed_numpy_random_funcs_max_try > rq4_amax.out
